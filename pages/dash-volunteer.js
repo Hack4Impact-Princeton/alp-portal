8cc3b765f15a6c93d717f7b534da9618f0f28019
Emily Luo <emilyluo555@gmail.com>
2023-03-21 17:36:25 -0400

    Co-authored-by: 03yangl <03yangl@users.noreply.github.com>

--- pages/dash-volunteer.js
+++ pages/dash-volunteer.js
@@ -16,18 +16,16 @@ function DashVolunteer() {
           justifyContent="center"
           alignItems="center"
           spacing={5}>
-            <Grid container columnSpacing={5}>
-              <Grid>
-                <div style={{fontSize: '20px', backgroundColor: 'yellow'}}>Welcome to ALP Portal!</div>
-                <div style={{fontSize: '20px', backgroundColor: 'red'}}>Active Drives</div>
-              </Grid>
-            </Grid>
+            <div style={{marginTop: '2vh'}}>
+              <div style={{fontSize: '35px', textAlign:'center'}}>Welcome to ALP Portal!</div>
+              <div style={{fontSize: '25px', textAlign:'center', marginTop: '2vh'}}>Active Drives</div>
+            </div>  
           <Stack
             direction="column"
             justifyContent="center"
             alignItems="center"
             spacing={20} sx={{backgroundColor:'yellow'}}>
-              <div style={{backgroundColor:"blue"}}>Placeholder 1</div>
+              <div /*style={{backgroundColor:"blue"}}*/>Placeholder 1</div>
               <div style={{backgroundColor:"blue"}}>Placeholder 2</div>
               <div style={{backgroundColor:"blue"}}>Placeholder 3</div>
               <div style={{backgroundColor:"blue"}}>Placeholder 4</div>
