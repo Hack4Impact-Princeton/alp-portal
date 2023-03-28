d8fc0b3988c60c63e8c3a2299b8f4fc8d32b4c93
Ivy Wang <iwang@princeton.edu>
2023-03-28 18:13:55 -0400

    Refactor DashviewDriveCard into DriveCard and DriveCardFront. There will be need for a DriveCardInfo or something, as clicking a button will push different text to the front.

--- pages/dash-volunteer.js
+++ pages/dash-volunteer.js
@@ -1,13 +1,12 @@
 import Box from '@mui/material/Box';
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import DashviewDriveCard from '../components/DashviewDriveCard'
+import DriveCard from '../components/DriveCard'
 import Grid from '@mui/material/Grid'; // Grid version 1
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Drawer from '@mui/material';
 import Stack from '@mui/material/Stack';
 
-// import dbConnect from '../lib/dbConnect'
 
 function DashVolunteer() {
   return (
@@ -26,10 +25,12 @@ function DashVolunteer() {
             justifyContent="center"
             alignItems="center"
             container spacing={10}>
-              <DashviewDriveCard></DashviewDriveCard>
-              <DashviewDriveCard></DashviewDriveCard>
-              <DashviewDriveCard></DashviewDriveCard>
-              <DashviewDriveCard></DashviewDriveCard>
+              <DriveCard
+                drivename="Uganda Community College"
+              ></DriveCard>
+              <DriveCard></DriveCard>
+              <DriveCard></DriveCard>
+              <DriveCard></DriveCard>
           </Stack>
         </Stack>
      
