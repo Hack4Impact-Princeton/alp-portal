6763b7cb65a524a896926b7a0691264589a6249c
Ivy Wang <iwang@princeton.edu>
2023-03-29 12:05:20 -0400

    Organize pages with an /auth subfolder. Remove passHref from Navbar links, unnecessary. Unstage DashviewDriveCard

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
             anchor="left"
         >
             <List>
-                <Link href="/dash-volunteer" passHref>
+                <Link href="/dash-volunteer">
                     <ListItem key="Home" sx={{
                         alignItems: 'center',
                     }}>
@@ -38,7 +38,7 @@ export default function Navbar() {
                         </ListItemButton>
                     </ListItem>
                 </Link>
-                <Link href="/dash-volunteer" passHref>
+                <Link href="/dash-volunteer">
                     <ListItem key="Organizer Checklist">
                         <ListItemButton>
                             <ListItemIcon>
@@ -49,7 +49,7 @@ export default function Navbar() {
                         </ListItemButton>
                     </ListItem>
                 </Link>
-                <Link href="/leaderboard" passHref>
+                <Link href="/leaderboard">
                     <ListItem key="Milestones">
                         <ListItemButton>
                             <ListItemIcon>
@@ -60,7 +60,7 @@ export default function Navbar() {
                         </ListItemButton>
                     </ListItem>
                 </Link>
-                <Link href="/h4i-team" passHref>
+                <Link href="/h4i-team">
                     <ListItem key="Contact Us">
                         <ListItemButton>
                             <ListItemIcon>
@@ -71,7 +71,7 @@ export default function Navbar() {
                         </ListItemButton>
                     </ListItem>
                 </Link>
-                <Link href="/forum" passHref>
+                <Link href="/forum">
                     <ListItem key="Forum">
                         <ListItemButton>
                             <ListItemIcon>
