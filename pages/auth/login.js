95e5130948ad4b79e0f4fe7f369ddab6568d21a8
Harbin Hong <harbinh@princeton.edu>
2023-04-11 20:50:51 -0400

    login button shoudl be ok

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,84 +1,178 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import { ClientRequest } from 'http';
-import {useState } from 'react'
+// import '../css/style.css'
+// import '../css/form.css'
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { ClientRequest } from "http";
+import getVolunteerAccountModel from "../../models/VolunteerAccount";
+import dbConnect from "../../lib/dbConnect";
 
-const Login = () => {
+function Login(props) {
+  let drives = JSON.parse(props.drives);
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  let [disabled, setDisabled] = useState(true);
 
-    const handleSetEmail = (emailText) => {
-        setEmail(emailText.target.value)
+  let emailsToPwhashs = {};
+  for (let i = 0; i < drives.length; i++) {
+    emailsToPwhashs[drives[i]["email"]] = drives[i]["pwhash"];
+  }
+
+  function verifyLogin() {
+    if (login in emailsToPwhashs && emailsToPwhashs[login] == pwd) {
+      console.log("Good login");
+      setDisabled(false);
+    } else {
+      console.log("Bad login");
     }
-    const handleSetPassword = (passwordText) => {
-        setPassword(passwordText.target.value)
-    }
+  }
 
-    const signUpHandler = async () => {
-        try {
-            const data = {email: email, password: password}
-            await fetch('/api/volunteeraccounts', {
-                method: "POST",
-                body: JSON.stringify(data),
-            });
-        } catch (e) {
-            console.error(e)
-        }
-    }
+  const [login, setLogin] = useState("NA");
+  const changeLogin = (event) => {
+    setLogin(event.target.value);
+  };
 
+  const [pwd, setPwd] = useState("NA");
+  const changePwd = (event) => {
+    setPwd(event.target.value);
+  };
+
+  if (disabled) {
     return (
-        <div>
+      <div>
         {/* TODO: <img src="" alt="ALP-logo"/> */}
         <h2> ALP Volunteer Portal Login </h2>
-        <Box 
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            sx={{   
-                width: 400,
-                height: 400,
-                backgroundColor: 'white',
-                border: 3,
-                borderColor: 'orange',
-        }}>
-            <Box
-                textAlign="center"
-                sx={{
-                    width: 300,
-                    height: 300,
-                }}
-                >
-                <TextField fullWidth required id="email" label="Email" variant="outlined" 
-                    value={email}
-                    onChange={handleSetEmail}
-                    sx={{
-                        mt: 2,
-                        mb: 2
-                    }}/>
-                <TextField fullWidth 
-                    required id="password" label="Password" variant="outlined" 
-                    value={password} onChange={handleSetPassword}
-                    sx={{
-                        mt: 2,
-                        mb: 2
-                    }}/>
-                <Button variant="contained"
-                    sx={{
-                        marginTop: 3,
-                    }}>Login</Button>
-                <Button variant="contained"
-                    onClick={signUpHandler}
-                    sx={{
-                        marginTop: 3,
-                        marginLeft: 3,
-                    }}>Sign Up</Button>
-            </Box>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            width: 400,
+            height: 400,
+            backgroundColor: "white",
+            border: 3,
+            borderColor: "orange",
+          }}
+        >
+          <Box
+            textAlign="center"
+            sx={{
+              width: 300,
+              height: 300,
+            }}
+          >
+            <TextField
+              onChange={changeLogin}
+              fullWidth
+              required
+              id="email"
+              label="Email"
+              variant="outlined"
+              sx={{
+                mt: 2,
+                mb: 2,
+              }}
+            />
+            <TextField
+              onChange={changePwd}
+              fullWidth
+              required
+              id="password"
+              label="Password"
+              variant="outlined"
+              sx={{
+                mt: 2,
+                mb: 2,
+              }}
+            />
+            <Button
+              onClick={verifyLogin}
+              variant="contained"
+              sx={{
+                marginTop: 3,
+              }}
+            >
+              Login
+            </Button>
+          </Box>
         </Box>
-        </div>
-    )
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {/* TODO: <img src="" alt="ALP-logo"/> */}
+        <h2> ALP Volunteer Portal Login </h2>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            width: 400,
+            height: 400,
+            backgroundColor: "white",
+            border: 3,
+            borderColor: "orange",
+          }}
+        >
+          <Box
+            textAlign="center"
+            sx={{
+              width: 300,
+              height: 300,
+            }}
+          >
+            <TextField
+              onChange={changeLogin}
+              fullWidth
+              required
+              id="email"
+              label="Email"
+              variant="outlined"
+              sx={{
+                mt: 2,
+                mb: 2,
+              }}
+            />
+            <TextField
+              onChange={changePwd}
+              fullWidth
+              required
+              id="password"
+              label="Password"
+              variant="outlined"
+              sx={{
+                mt: 2,
+                mb: 2,
+              }}
+            />
+            <Button
+              onClick={verifyLogin}
+              variant="contained"
+              sx={{
+                marginTop: 3,
+              }}
+            >
+              Login
+            </Button>
+
+            <Button href="/dash-volunteer">Click here</Button>
+          </Box>
+        </Box>
+      </div>
+    );
+  }
 }
 
-export default Login
+/* Keep example code here, nothing should be dynamic on the home page */
+export async function getServerSideProps() {
+  await dbConnect();
+  const volunteerAccount = getVolunteerAccountModel();
+  /* find all the data in our database */
+  const drives = await volunteerAccount.find({});
+  // stringify data before sending
+  return { props: { drives: JSON.stringify(drives) } };
+}
+/* end example pet code */
+
+export default Login;
