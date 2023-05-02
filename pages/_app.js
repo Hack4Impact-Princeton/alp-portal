0d671b318c8b8e482fc8ed415650e0ae25179fbb
Emily Luo <emilyluo555@gmail.com>
2023-05-02 17:41:04 -0400

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,22 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Logo from '../components/Logo'
 import { useEffect, useState } from 'react'
-import useContentful from './useContentful'
+/*import useContentful from './useContentful'*/
 
 function MyApp({ Component, pageProps }) {
   /* Hooks for Contentful logos */
+/*  
   const [logos, setLogos] = useState([]);
   const { getLogos } = useContentful();
   /* Logos are rendered in the bottommost div in the return */
-
+  
   /* Use this to see what getLogos outputs:
-  getLogos().then((response) => console.log(response)); */
+  getLogos().then((response) => console.log(response)); 
 
   useEffect(() => {
     getLogos().then((response) => setLogos(response));
   });
-
+*/
   return (
     <>
       <Head>
