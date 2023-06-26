3bb3cf0438b7ac95d924ee720b586da0a194ed7a
crimmzx <111248873+crimmzx@users.noreply.github.com>
2023-06-26 14:20:48 -0500

    added the handlesubmit function

--- components/InstructionInputCard.jsx
+++ components/InstructionInputCard.jsx
@@ -9,6 +9,25 @@ export default function InstructionInputCard({ heading, subHeading, submitBtn, s
     setValue(event.target.value);
   };
 
+  const handleSubmit = async () => {
+    try {
+      const data = {
+        gs: {
+          fundraise: value,
+          terms: true, // Assuming you want to set 'terms' to true
+        },
+      };
+      console.log("data: ", JSON.stringify(data));
+      await fetch(`/api/bookDrive/${props.driveCode}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+      });
+      console.log("done");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Grid
       sx={{
@@ -23,39 +42,40 @@ export default function InstructionInputCard({ heading, subHeading, submitBtn, s
     >
       <Grid item xs={12}>
         <Typography variant="h4">
-          <span>How are you fundraising</span>
+          <span>How Will You Fundraise?</span>
         </Typography>
         <Typography display="block"> </Typography>
-        <Typography variant="h6">
           <span>See fundraising ideas here</span>
+        <Typography variant="h6">
+          <span>{subHeading}</span>
         </Typography>
       </Grid>
 
-      <CardBody value={value} handleChange={handleChange} />
+      <CardBody value={value} handleChange={handleChange} handleSubmit={handleSubmit} />
     </Grid>
   );
 }
 
-function CardBody({ value, handleChange }) {
+function CardBody({ value, handleChange, handleSubmit }) {
   return (
     <Grid container alignItems="center" sx={{ p: 5 }}>
-    <Grid item xs={12} sx={{ pb: 3 }}>
-      <TextField
-        value={value}
-        multiline
-        rows={5} // Adjust the number of rows to make the box taller
-        fullWidth
-        id="books-collected"
-        variant="outlined"
-        onChange={handleChange}
-      />
-    </Grid>
+      <Grid item xs={12} sx={{ pb: 3 }}>
+        <TextField
+          value={value}
+          multiline
+          rows={5} // Adjust the number of rows to make the box taller
+          fullWidth
+          id="books-collected"
+          variant="outlined"
+          onChange={handleChange}
+        />
+      </Grid>
 
-    <Grid item xs={11}>
-      <Button variant="contained" size="medium">
-        Submit
-      </Button>
+      <Grid item xs={11}>
+        <Button variant="contained" size="medium" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
 }
