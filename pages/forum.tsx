f897b0a020a257e919869c894e5920b9dff98fa3
Emily Luo <emilyluo555@gmail.com>
2023-10-04 20:40:02 -0400

    sample search bar component made

--- pages/forum.tsx
+++ pages/forum.tsx
@@ -1,11 +1,34 @@
-import { NextPage } from "next"
-import Navbar from "../components/Navbar"
+import { NextPage } from "next";
+import Navbar from "../components/Navbar";
+import SearchBar from "../components/forum/SearchBar";
+import Box from "@mui/material/Box";
+import PageContainer from "../components/PageContainer";
+import DriveCard from "../components/DriveCard";
+import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
+import useDynamicPadding from "../lib/useDynamicPadding";
+
 const Forum: NextPage = () => {
-  return <div>
-    <Navbar active="forum" />
-    <p>Forum</p>
+  return (
+    <div>
+      <Grid2>
+        <Navbar active="forum" />
+        {/* Necessary box for padding the page body, no overlap with Navbar */}
+        <Box
+          display="flex"
+          flexDirection="column"
+          sx={{
+            pl: useDynamicPadding(635, 775, "29vw", "20vw", "15vw"),
+            pt: "5vh",
+            pr: "5vw",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <SearchBar />
+        </Box>
+      </Grid2>
+    </div>
+  );
+};
 
-  </div>
-}
-
-export default Forum
+export default Forum;
