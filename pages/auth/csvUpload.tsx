64fe8277cd17dc8b0ace24c064ebf75b9b95d026
Harbin Hong <hhong61502@gmail.com>
2023-10-17 12:43:21 -0400

    testCsvUploadBranch

---
+++ pages/auth/csvUpload.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import * as d3 from "d3";
+
+const Upload = () => {
+  const [uploading, setUploading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState();
+  const [json, setJson] = useState({ body: "" });
+
+  // set uploaded csv file as selectedFile
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  // go inside csv file and extract (for now) first book drive
+  const handleUploadCSV = () => {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const csvData = event.target.result;
+
+      // Use D3.js to parse the CSV data
+      const parsedData = d3.csvParse(csvData);
+
+      // Now you can work with the parsed data
+      console.log(parsedData);
+
+      const testDataCase = parsedData[0];
+
+      let testBookDriveJson = {
+        body: `{
+            "driveName": "${testDataCase["Book Drive Name"]}",
+            "driveCode": "${testDataCase["Book Drive Code"]}",
+            "organizer": "${testDataCase["Contact: Full Name"]}",
+            "startDate": "${new Date()}",
+            "country": "USA",
+            "status": "Active",
+            "booksGoal": 1000,
+            "completedDate": "${new Date()}",
+            "mailDate": "${new Date()}",
+            "reactivationRequestId": null,
+            "gs": {
+              "fundraise": "fundraise",
+              "terms": true
+            },
+            "cb": {
+              "booksCurrent": 0,
+              "updateFreq": 0,
+              "lastUpdate": "${new Date()}"
+            },
+            "pts": {
+              "intFee": 0,
+              "domFee": 0,
+              "materials": {
+                "boxes": false,
+                "extraCardboard": false,
+                "tape": false,
+                "mailingLabels": false
+              }
+            },
+            "fl": {
+              "isFinalized": false,
+              "shipments": []
+            }
+          }`,
+      };
+      console.log(testBookDriveJson);
+      setJson(testBookDriveJson);
+      // Perform any further processing or rendering with the parsed data here
+    };
+
+    reader.readAsText(selectedFile);
+  };
+
+  return (
+    <div>
+      <div>
+        <h4 className="page-header mb-4">Upload a CSV</h4>
+        <div className="mb-4">
+          <input
+            disabled={uploading}
+            type="file"
+            className="form-control"
+            onChange={changeHandler}
+          />
+        </div>
+        <button
+          onClick={handleUploadCSV}
+          disabled={uploading}
+          className="btn btn-primary"
+        >
+          {uploading ? "Uploading..." : "Upload"}
+        </button>
+      </div>
+      <div>
+        <h4> Testing Testing </h4>
+      </div>
+    </div>
+  );
+};
+
+Upload.propTypes = {};
+
+export default Upload;
