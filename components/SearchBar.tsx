a402f4384128a728a16174e42e702e4513e8d5fa
crimmzx <wz6624@princeton.edu>
2023-11-05 15:13:59 -0500

    serach bar is cookin

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import SearchIcon from '@mui/icons-material/Search'; // Import the Search icon from Material-UI
 import { VolunteerAccount } from '../models/VolunteerAccount';
 
 const SearchBar: React.FC<{
@@ -14,17 +15,40 @@ const SearchBar: React.FC<{
     onQueryChange(query, filteredUsers);
   }, [query]);
 
+  const searchBarStyle = {
+    position: 'absolute',
+    left: '250px', // Adjust the right position as needed
+    top: '200px', // Adjust the top position as needed
+    width: '300px', // Adjust the width as needed
+    fontSize: '16px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    outline: 'none',
+  };
+
+  const inputStyle = {
+    width: '100%', // Adjust the width as needed
+    padding: '10px',
+    fontSize: '16px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    outline: 'none',
+    paddingLeft: '40px', // To make space for the icon
+  };
+
+  const iconStyle = {
+    position: 'absolute',
+    left: '10px', // Adjust the left position to your preference
+    top: '50%', // Center vertically
+    transform: 'translateY(-50%)',
+    color: '#888', // Icon color
+  };
+
   return (
-    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+    <div style={searchBarStyle}>
+      <SearchIcon style={iconStyle} /> {/* Add the Search icon */}
       <input
-        style={{
-          width: '300px', // Adjust the width as needed
-          padding: '10px',
-          fontSize: '16px',
-          border: '1px solid #ccc',
-          borderRadius: '5px',
-          outline: 'none',
-        }}
+        style={inputStyle}
         type="text"
         placeholder="Search for users"
         value={query}
--- pages/profile_search.tsx
+++ pages/profile_search.tsx
@@ -27,6 +27,81 @@ type ProfileProps = {
   drives: BookDrive[] | null;
   broadcasts: Broadcast[];
 }
+type BadgeInfoProps = {
+  isEarned: boolean,
+  level: number,
+  name: string,
+  description: string,
+}
+const BadgeInfo: React.FC<BadgeInfoProps> = ({ isEarned, level, name, description }) => {
+  const unlockedBadgeStyle = {
+    width: '40px',
+    height: '40px',
+    marginBottom: '5px',
+  };
+
+  const lockedBadgeStyle = {
+    width: '40px',
+    height: '40px',
+    marginBottom: '5px',
+    filter: 'grayscale(100%)',
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+      {isEarned ? (
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1435/1435715.png"
+          alt="Unlocked Badge"
+          style={unlockedBadgeStyle}
+        />
+      ) : (
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1435/1435722.png"
+          alt="Locked Badge"
+          style={lockedBadgeStyle}
+        />
+      )}
+      <div style={{ textAlign: 'center' }}>
+        <p style={{ margin: 0, fontWeight: 'bold' }}>{name}</p>
+        <p style={{ margin: 0, fontSize: '12px' }}>{description}</p>
+      </div>
+    </div>
+  );
+};
+const BadgeDisplayCase = () => {
+  const badges = [
+    {
+      isEarned: true,
+      level: 1,
+      name: 'Ivy',
+      description: 'ivy being nice for once',
+    },
+    {
+      isEarned: false,
+      level: 2,
+      name: 'ivy',
+      description: 'ivy saving us!',
+    },
+    // Add more badges here
+  ];
+
+  return (
+    <Grid container style={{
+      border: '1.5px solid black', padding: '10px', marginBottom: '10px', display: 'flex',
+      width: '95%',
+      backgroundColor: "#F5F5F5"
+
+    }}>
+      <Grid xs={12} ><h2 style={{ textAlign: 'left', marginBottom: '10px' }}>Badges</h2></Grid>
+      <Grid container xs={12} style={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap', paddingLeft: '10px' }}>
+        {badges.map((badge, index) => (
+          <BadgeInfo key={index} {...badge} />
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
 const profile_search: NextPage<ProfileProps> = ({ error, broadcasts, account, drives }) => {
   console.log("Profile Page");
 
