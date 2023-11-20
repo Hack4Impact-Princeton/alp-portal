429590301221fd601cffccb70d519b4e7a0b4ce5
crimmzx <wz6624@princeton.edu>
2023-11-19 20:16:20 -0500

    css

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -8,15 +8,15 @@ type BadgeInfoProps = {
 };
 
 type ProfileCardProps = {
-    name: string;
+  name: string;
   state: string;
   email: string;
   profilePicture: string;
   badges: BadgeInfoProps[];
   useBadges?: boolean;
-  friendStatus: string; // Add friendStatus prop
-  onFriendRequest: () => void; // Add onFriendRequest prop
-  onRevokeFriendRequest: () => void; // Add onRevokeFriendRequest prop
+  friendStatus: string;
+  onFriendRequest: () => void;
+  onRevokeFriendRequest: () => void;
   style?: React.CSSProperties;
 };
 
@@ -24,18 +24,27 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ name, state, email, profilePi
   const [friendStatus, setFriendStatus] = React.useState<string>('none');
   const [showRevokeButton, setShowRevokeButton] = React.useState<boolean>(false);
 
-const cardStyle: React.CSSProperties = {
+  const cardStyle: React.CSSProperties = {
     border: '1px solid #ddd',
     padding: '10px',
     marginBottom: '10px',
     width: '300px',
     display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    backgroundColor: '#f9f9f9'
+  };
+
+  const contentStyle: React.CSSProperties = {
+    marginLeft: '10px',
+    display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
+    flex: 1, // Let the content take the available space
   };
 
   const imageStyle: React.CSSProperties = {
-    width: '100%',
+    width: '100px', // Adjust the width as needed
     height: 'auto',
     marginBottom: '10px',
   };
@@ -48,59 +57,50 @@ const cardStyle: React.CSSProperties = {
     marginBottom: '10px',
   };
 
-const buttonsContainerStyle: React.CSSProperties = {
-  marginTop: '10px', // Add margin to separate buttons from badges
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  width: '100%', // Ensure the container takes the full width
-};
-const getButtonColor = () => {
-  switch (friendStatus) {
-    case 'none':
-      return '#FE9834'; // Color for "Send Friend Request"
-    case 'sent':
-    case 'received':
-      return '#FF6347'; // Color for "Revoke Friend Request"
-    case 'friends':
-      return '#4CAF50'; // Color for "Unfriend"
-    default:
-      return '#FE9834'; // Default color (you can change this)
-  }
-};
-const buttonStyle: React.CSSProperties = {
-  position: 'relative',
-  padding: '8px 16px',
-  borderRadius: '4px',
-  border: 'none',
-  backgroundColor: getButtonColor(),
-  color: 'white',
-  cursor: 'not-allowed',
-  margin: '4px 0',
-};
+  const buttonsContainerStyle: React.CSSProperties = {
+    marginTop: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
 
-const revokeButtonStyle: React.CSSProperties = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  width: '100%',
-  height: '100%',
-  borderRadius: '4px',
-  backgroundColor: '#A9A9A9', // Gray color for revoke button
-  color: 'white',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  pointerEvents: 'none', // Disable pointer events for the 'x' icon
-};
+  const getButtonColor = () => {
+    switch (friendStatus) {
+      case 'none':
+        return '#FE9834';
+      case 'sent':
+      case 'received':
+        return '#FF6347';
+      case 'friends':
+        return '#4CAF50';
+      default:
+        return '#FE9834';
+    }
+  };
 
-const revokeIconStyle: React.CSSProperties = {
-  position: 'absolute',
-  top: '50%',
-  left: '4px', // Adjust the left positioning
-  transform: 'translateY(-50%)',
-  cursor: 'pointer',
-};
+  const buttonStyle: React.CSSProperties = {
+    padding: '8px 16px',
+    borderRadius: '4px',
+    border: 'none',
+    backgroundColor: getButtonColor(),
+    color: 'white',
+    cursor: 'pointer',
+    margin: '4px 0',
+  };
+
+  const revokeButtonStyle: React.CSSProperties = {
+    padding: '8px 16px',
+    borderRadius: '4px',
+    backgroundColor: '#A9A9A9',
+    color: 'white',
+    cursor: 'pointer',
+    margin: '4px 0',
+  };
+
+  const revokeIconStyle: React.CSSProperties = {
+    marginLeft: '4px', // Adjust the left positioning
+    cursor: 'pointer',
+  };
 
   const handleSendFriendRequest = () => {
     setFriendStatus('sent');
@@ -116,80 +116,67 @@ const revokeIconStyle: React.CSSProperties = {
     setShowRevokeButton(true);
   };
 
-  return (
+return (
     <div style={cardStyle}>
       <img src={profilePicture} alt="Profile" style={imageStyle} />
-      <p>Name: {name}</p>
-      <p>State: {state}</p>
-      <p>Email: {email}</p>
+      <div style={contentStyle}>
+        <p>{name}</p>
+        <p>{state}</p>
+        <p>{email}</p>
         {useBadges && (
-        <>
-          <h3>Badges:</h3>
-          <div style={badgesContainerStyle}>
-            {badges.map((badge, index) => (
-              <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
-                {badge.isEarned ? (
-                  <img src="https://cdn-icons-png.flaticon.com/512/1435/1435715.png" alt="Unlocked Badge" style={{ width: '40px', height: '40px', marginBottom: '5px' }} />
-                ) : (
-                  <img src="https://cdn-icons-png.flaticon.com/512/1435/1435722.png" alt="Locked Badge" style={{ width: '40px', height: '40px', marginBottom: '5px', filter: 'grayscale(100%)' }} />
-                )}
-                <div style={{ textAlign: 'center' }}>
-                  <p style={{ margin: 0, fontWeight: 'bold' }}>{badge.name}</p>
-                  <p style={{ margin: 0, fontSize: '12px' }}>{badge.description}</p>
+          <>
+            <h3>Badges:</h3>
+            <div style={badgesContainerStyle}>
+              {badges.map((badge, index) => (
+                <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+                  {badge.isEarned ? (
+                    <img src="https://cdn-icons-png.flaticon.com/512/1435/1435715.png" alt="Unlocked Badge" style={{ width: '40px', height: '40px', marginBottom: '5px' }} />
+                  ) : (
+                    <img src="https://cdn-icons-png.flaticon.com/512/1435/1435722.png" alt="Locked Badge" style={{ width: '40px', height: '40px', marginBottom: '5px', filter: 'grayscale(100%)' }} />
+                  )}
+                  <div style={{ textAlign: 'center' }}>
+                    <p style={{ margin: 0, fontWeight: 'bold' }}>{badge.name}</p>
+                    <p style={{ margin: 0, fontSize: '12px' }}>{badge.description}</p>
+                  </div>
                 </div>
+              ))}
+            </div>
+          </>
+        )}
+        <div style={buttonsContainerStyle}>
+          {friendStatus === 'none' && (
+            <button onClick={handleSendFriendRequest} style={buttonStyle}>
+              Send Friend Request
+            </button>
+          )}
+          {friendStatus === 'sent' && (
+            <div style={{ position: 'relative', width: '100%' }}>
+              <div style={revokeButtonStyle}>
+                Revoke
+                <span
+                  style={revokeIconStyle}
+                  onClick={() => {
+                    handleRevokeFriendRequest();
+                    setFriendStatus('none');
+                  }}
+                >
+                  x
+                </span>
               </div>
-            ))}
-
-       {/* Friend Request Buttons */}
-    <div style={buttonsContainerStyle}>
-      {friendStatus === 'none' && (
-        <button
-          onClick={handleSendFriendRequest}
-          style={{
-            backgroundColor: '#FE9834',
-            color: '#fff',
-            padding: '5px 10px',
-            cursor: 'pointer',
-            border: 'none', // Remove the default button border
-            outline: 'none', // Remove the default button outline
-            // Add other styles as needed
-          }}
-        >
-          Send Friend Request
-        </button>
-      )}
-      {friendStatus === 'sent' && (
-        <div style={{ position: 'relative', width: '100%' }}>
-          <div style={buttonStyle}>
-            Revoke
-            <span
-              style={revokeIconStyle}
-              onClick={() => {
-                handleRevokeFriendRequest();
-                // Explicitly set the friendStatus state to 'none' when 'x' is clicked
-                setFriendStatus('none');
-              }}
-            >
-              x
-            </span>
-          </div>
+            </div>
+          )}
+          {(friendStatus === 'received' || friendStatus === 'friends') && showRevokeButton && (
+            <button style={buttonStyle} onClick={handleRevokeFriendRequest}>
+              Unfriend
+            </button>
+          )}
+          {friendStatus === 'received' && (
+            <button style={buttonStyle} onClick={handleAcceptFriendRequest}>
+              Accept Friend Request
+            </button>
+          )}
         </div>
-      )}
-      {(friendStatus === 'received' || friendStatus === 'friends') && showRevokeButton && (
-        <button style={buttonStyle} onClick={handleRevokeFriendRequest}>
-          Unfriend
-        </button>
-      )}
-      {friendStatus === 'received' && (
-        <button style={buttonStyle} onClick={handleAcceptFriendRequest}>
-          Accept Friend Request
-        </button>
-      )}
-    </div>
-  </div>
-        </>
-      )}
-      {/* Add other profile card content as needed */}
+      </div>
     </div>
   );
 };
