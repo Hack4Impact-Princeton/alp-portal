ad5d243e5dac3b75ea3a0116913b3eb5da1ab637
EmilioC1 <ec2736@princeton.edu>
2023-12-07 23:42:46 -0500

    finished chat for now

--- components/CircularIcon.tsx
+++ components/CircularIcon.tsx
@@ -1,7 +1,7 @@
 
-const CircularIcon: React.FC<{ stringContent?: string | null, reactNodeContent?: React.ReactNode | null, bgColor: string, fgColor?: string, diameter?: string }> = ({ stringContent, reactNodeContent, bgColor, fgColor, diameter }) => {
+const CircularIcon: React.FC<{ stringContent?: string | null, reactNodeContent?: React.ReactNode | null, bgColor: string, fgColor?: string, diameter?: string, onClick?: () => void }> = ({ stringContent, reactNodeContent, bgColor, fgColor, diameter, onClick }) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={diameter ? diameter : "28"} height={diameter ? diameter : "28"} viewBox="0 0 28 28" fill="none">
+        <svg xmlns="http://www.w3.org/2000/svg" onClick={onClick} width={diameter ? diameter : "28"} height={diameter ? diameter : "28"} viewBox="0 0 28 28" fill="none">
             <circle cx="14" cy="14" r="14" fill={bgColor} />
             {reactNodeContent && reactNodeContent}
             {stringContent &&
@@ -34,10 +34,3 @@ const CircularIcon: React.FC<{ stringContent?: string | null, reactNodeContent?:
 }
 
 export default CircularIcon
-
-/* 
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" viewBox="0 0 16 12" fill="none">
-<rect x="7" y="10" width="14" height="9" stroke="#5F5F5F" stroke-width="2" />
-<path d="M12.80216 15.82011C13.19047 16.19844 13.80953 16.19844 14.19784 15.82011L18.6666 11.46626C19.3093 10.84016 18.866 9.75 17.9688 9.75H9.0312C8.134 9.75 7.69074 10.84016 8.33337 11.46626L12.80216 15.82011Z" stroke="#5F5F5F" stroke-width="2" />
-</svg>
-*/
--- components/forum/Message.tsx
+++ components/forum/Message.tsx
@@ -1,10 +1,74 @@
 import { Message } from "../../models/Chat"
+import format from "date-fns/format";
+import formatDistance from "date-fns/formatDistance";
+import isSameDay from "date-fns/isSameDay";
 
-const MessageComponent: React.FC<{ message: Message, isCurrUserMessage: boolean }> = ({ message, isCurrUserMessage }) => {
+const MessageComponent: React.FC<{ message: Message, isCurrUserMessage: boolean, otherUserName: string }> = ({ message, isCurrUserMessage, otherUserName }) => {
+    // const userIcon = <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //     <g id="Group 75">
+    //         <circle id="Ellipse 10" cx="25" cy="25" r="25" fill="#FE9835" />
+    //         <ellipse id="Ellipse 11" cx="25" cy="16" rx="7.858" ry="7.112" fill="white" />
+    //         <path id="Ellipse 12" d="M36.0896 32.2832C36.0896 35.9743 30.7314 35.5332 25 35.5332C19.2686 35.5332 13.9104 35.9743 13.9104 32.2832C13.9104 28.5921 19.2686 25.55 25 25.55C30.7314 25.55 36.0896 28.5921 36.0896 32.2832Z" fill="white" />
+    //     </g>
+    // </svg>
+    const formatDate = (d: string) => {
+        const NATURAL_FORMATTING_DISTANCE = 30 * 6; // 6 months
+        const date = Date.parse(d);
+
+        const diff = Math.floor(
+            (new Date().getTime() - date) / (24 * 60 * 60 * 1000)
+        );
+
+        if (diff <= NATURAL_FORMATTING_DISTANCE) {
+            return formatDistance(date, new Date(), { addSuffix: true });
+        } else {
+            return format(date, "MMMM do, yyyy");
+        }
+    };
+    const userIcon = (
+        <svg width="37.5" height="37.5" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="Group 75">
+                <circle id="Ellipse 10" cx="25" cy="25" r="25" fill="#FE9835" />
+                <ellipse id="Ellipse 11" cx="25" cy="16" rx="7.858" ry="7.112" fill="white" />
+                <path
+                    id="Ellipse 12"
+                    d="M36.0896 32.2832C36.0896 35.9743 30.7314 35.5332 25 35.5332C19.2686 35.5332 13.9104 35.9743 13.9104 32.2832C13.9104 28.5921 19.2686 25.55 25 25.55C30.7314 25.55 36.0896 28.5921 36.0896 32.2832Z"
+                    fill="white"
+                />
+            </g>
+        </svg>
+    );
+
+    let date = formatDate(message.sentTime)
+    let showFullDate = false
+    if (date.substring(date.length - 8) === "days ago") {
+        showFullDate = true
+        date = message.sentTime.substring(0, 10)
+        if (date.substring(date.length - 1) === ',') date = date.substring(0, 9)
+    }
     return (
-        <div style={{ width: "wrap-content", height: "wrap-content", padding: 5, border: "1.5px solid black", borderRadius: "35%", backgroundColor: isCurrUserMessage ? "#1F8AFF" : "d8d8d8", alignSelf: isCurrUserMessage ? "flex-end" : "flex-start" }}>
-            <p style={{ fontSize: 20, fontWeight: 600 }}>{message.messageString}</p>
-            <p style={{ fontSize: 8 }}>{message.sentTime}</p>
+
+        <div style={{
+            display: "flex", flexDirection: "row", alignItems: "start", width: "100%", height: "wrap-content", backgroundColor: "#5F5F5F", paddingTop: "10px", padding: "1px", cursor: "pointer", marginBottom: "5px",
+        }}>
+            <div style={{ marginRight: "10px", marginLeft: "10px" }}>
+                {userIcon}
+            </div>
+            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "start", width: "100%" }}>
+                <h3 style={{ color: "white" }}>
+                    {isCurrUserMessage ? "You" : otherUserName}
+                </h3>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "start", marginRight: "3px", width: "100%" }}>
+                    <p style={{ textAlign: "left", color: "white", fontSize: 13, width: "wrap-content", height: "wrap-content", maxWidth: 200, flexWrap: "wrap", whiteSpace: "normal", wordWrap: "break-word", hyphens: "auto" }}>
+                        {message.messageString}
+                    </p>
+                    <p style={{ textAlign: "right", color: "white", fontSize: 6, maxWidth: 50 }}>
+                        {showFullDate && message.sentTime}
+                        {!showFullDate && date}
+                    </p>
+                </div>
+
+            </div>
         </div>
     )
 }
